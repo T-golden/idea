4d998211262b91bdf3c9ee7cbf0bc258abfc29f5
T-golden <752740545@qq.com>
2017-11-03 00:52:36 +0800

    2017-11-3 href

--- MySpring/src/main/java/com/example/demo/controller/TeacherController.java
+++ MySpring/src/main/java/com/example/demo/controller/TeacherController.java
@@ -8,100 +8,131 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/teacher")
 public class TeacherController {
-	
+
 	@RequestMapping("")
 	public String toIndex() {
 		return "teacher/teacher_index";
 	}
-	
+
 	/**
 	 * 创建学生账号
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/toCreateStudent")
-	public String toCreateStudent() {
+	public String toCreateStudent(@RequestParam("teacherId") String teacherId , Model model) {
+		System.out.println(teacherId);
+		model.addAttribute("teacherId", teacherId);
 		return "teacher/teacher_create_student";
 	}
-	
+
 	/**
 	 * 学生信息
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/toStudentInfo")
-	public String toStudentInfo() {
+	public String toStudentInfo(@RequestParam("teacherId") String teacherId , Model model) {
+		model.addAttribute("teacherId", teacherId);
 		return "teacher/teacher_student_info";
 	}
 	
 	/**
+	 * 创建班级
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/toCreateClass")
+	public String toCreateClass(@RequestParam("teacherId") String teacherId , Model model) {
+		model.addAttribute("teacherId", teacherId);
+		return "teacher/teacher_create_class";
+	}
+	
+	/**
+	 * 班级信息
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/toClassInfo")
+	public String toClassInfo(@RequestParam("teacherId") String teacherId , Model model) {
+		model.addAttribute("teacherId", teacherId);
+		return "teacher/teacher_class_info";
+	}
+
+	/**
 	 * 课程信息
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/tocourse")
-	public String tocourse() {
+	public String tocourse(@RequestParam("teacherId") String teacherId , Model model) {
+		model.addAttribute("teacherId", teacherId);
 		return "teacher/teacher_course";
 	}
-	
+
 	/**
 	 * 章节信息
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/toChapter")
-	public String toChapter() {
+	public String toChapter(@RequestParam("teacherId") String teacherId , Model model) {
+		model.addAttribute("teacherId", teacherId);
 		return "teacher/teacher_chapter";
 	}
-	
+
 	/**
 	 * 上传教程
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/toChapterUpload")
-	public String toChapterUpload() {
+	public String toChapterUpload(@RequestParam("teacherId") String teacherId , Model model) {
+		model.addAttribute("teacherId", teacherId);
 		return "teacher/teacher_chapter_material_upload";
 	}
-	
+
 	/**
 	 * 教程信息
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/toChapterMaterial")
-	public String toChapterMaterial() {
+	public String toChapterMaterial(@RequestParam("teacherId") String teacherId , Model model) {
+		model.addAttribute("teacherId", teacherId);
 		return "teacher/teacher_chapter_material";
 	}
-	
+
 	/**
 	 * 作业上传
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/toAssignmentUpload")
-	public String toAssignmentUpload() {
+	public String toAssignmentUpload(@RequestParam("teacherId") String teacherId , Model model) {
+		model.addAttribute("teacherId", teacherId);
 		return "teacher/teacher_assignment_upload";
 	}
-	
+
 	/**
 	 * 作业信息
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/toAssignmentInfo")
-	public String toAssignmentInfo() {
+	public String toAssignmentInfo(@RequestParam("teacherId") String teacherId , Model model) {
+		model.addAttribute("teacherId", teacherId);
 		return "teacher/teacher_assignment_info";
 	}
-	
+
 	/**
 	 * 修改密码
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/toUpdatePass")
-	public String toUpdatePass() {
+	public String toUpdatePass(@RequestParam("teacherId") String teacherId , Model model) {
+		model.addAttribute("teacherId", teacherId);
 		return "teacher/teacher_updatePass";
-	}
-	
-	
-	
-	
-	
-	
-	@RequestMapping("createStudent")
-	public String createStudent(@RequestParam("teacherId") String teacherId , @RequestParam("num") int num , Model model) {
-		return "";
 	}
 }
